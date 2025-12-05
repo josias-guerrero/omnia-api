7d80c10f669db1ce2ebe85fb9c7b7f7d94a0a169
Josias <josiasguerrero2004@gmail.com>
2025-12-05 09:54:58 -0600

    First commit, base application from spring initializer with already configured some properties and docker-compsoe

---
+++ src/main/java/org/josiasguerrero/omnia_api/OmniaApiApplication.java
@@ -0,0 +1,13 @@
+package org.josiasguerrero.omnia_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmniaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/josiasguerrero/omnia_api/OmniaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.josiasguerrero.omnia_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmniaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
